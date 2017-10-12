23971ca6fbb2481ec5ad23f0480df8bb9bae8655
Mankirat Singh <mankirat1998@gmail.com>
2017-10-12 20:11:31 +0530

    feat: Register added

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 "use strict";
-var express= require("express");
-var mongoose= require("mongoose");
+var express               = require("express"),
+    mongoose              = require("mongoose"),
+    passport              = require("passport"),
+    bodyParser            = require("body-parser"),
+    User                  = require("./models/user"),
+    LocalStrategy         = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose");
 
 mongoose.connect("mongodb://localhost/auth_demo");
+
 var app= express();
-
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(require("express-session")({
+    secret:"This is my secret better beware",
+    resave:false,
+    saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+//ROUTES
 
 app.get("/", function (req, res) {
     res.render("home");
@@ -15,6 +35,25 @@ app.get("/", function (req, res) {
 app.get("/secret", function (req, res) {
     res.render("secret");
 })
+
+// AUTH ROUTES
+
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+app.post("/register", function (req, res) {
+    User.register(new User({username: req.body.username}), req.body.password, function (err, user) {
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }else{
+            passport.authenticate("local")(req,res,function () {
+                res.redirect("secret");
+            });
+        }
+    });
+});
 
 
 app.listen(4000, function () {
