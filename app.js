fd9ec6c7644660c7fc9d260cdffe143cae3a639d
Mankirat Singh <mankirat1998@gmail.com>
2017-10-12 22:53:07 +0530

    feat: Logout added

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.get("/", function (req, res) {
     res.render("home");
 });
 
-app.get("/secret", function (req, res) {
+app.get("/secret", isLoggedIn, function (req, res) {
     res.render("secret");
 })
 
@@ -70,6 +70,20 @@ app.post("/login",passport.authenticate("local",{
 }) ,function (req, res) {
 
 });
+
+//LOGOUT ROUTE
+
+app.get("/logout", function (req, res) {
+    req.logOut();
+    res.redirect("/");
+});
+
+function isLoggedIn(req,res,next) {
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(4000, function () {
     console.log("Auth app is online");
